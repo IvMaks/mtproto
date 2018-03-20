19e599bba1b3a73cc76ae5ac2e19f162181d11bd
Maksim Ivanyukhin <ivmaks@gmail.com>
2018-03-20 21:18:50 +0300

    fix merge

--- channels.go
+++ channels.go
@@ -3,7 +3,7 @@ package mtproto
 import "fmt"
 
 func (m *MTProto) GetChannels() (channels []TL_channel, err error) {
-        if dialogs, err := m.MessagesGetDialogs(true, int32(0), int32(0), TL_inputPeerEmpty{}, int32(100)); err == nil {
+	if dialogs, err := m.MessagesGetDialogs(true, int32(0), int32(0), TL_inputPeerEmpty{}, int32(100)); err == nil {
 		for _, chat := range (*dialogs).(TL_messages_dialogsSlice).Chats {
 			switch chat.(type) {
 			case TL_channel:
@@ -18,7 +18,7 @@ func (m *MTProto) GetChannels() (channels []TL_channel, err error) {
 }
 
 func (m *MTProto) GetMembers(channel, filter TL, off, limit int32) ([]TL, error) {
-        var resp = make(chan response, 1)
+	var resp = make(chan response, 1)
 	m.queueSend <- packetToSend{
 		msg: TL_channels_getParticipants{
 			Channel: channel,
@@ -39,62 +39,60 @@ func (m *MTProto) GetMembers(channel, filter TL, off, limit int32) ([]TL, error)
 		return nil, fmt.Errorf("Connection error: got: %T", x)
 	}
 }
-func (m *MTProto) ChannelsCreateChannel(broadcast,megagroup bool,title,about string) (*TL,error){
+func (m *MTProto) ChannelsCreateChannel(broadcast, megagroup bool, title, about string) (*TL, error) {
 	return m.InvokeSync(TL_channels_createChannel{
-		Broadcast:broadcast,
-		Megagroup:megagroup,
-		Title:title,
-		About:about,
+		Broadcast: broadcast,
+		Megagroup: megagroup,
+		Title:     title,
+		About:     about,
 	})
 }
 
-func (m *MTProto) ChannelsDeleteChannel(channel TL) (*TL,error){
+func (m *MTProto) ChannelsDeleteChannel(channel TL) (*TL, error) {
 	return m.InvokeSync(TL_channels_deleteChannel{
-		Channel:channel,
+		Channel: channel,
 	})
 }
 
-func (m *MTProto) ChannelsKickFromChannel(channel TL,userId TL, kicked TL) (*TL,error){
+func (m *MTProto) ChannelsKickFromChannel(channel TL, userId TL, kicked TL) (*TL, error) {
 	return m.InvokeSync(TL_channels_kickFromChannel{
-		Channel:channel,
-		User_id:userId,
-		Kicked:kicked,
+		Channel: channel,
+		User_id: userId,
+		Kicked:  kicked,
 	})
 }
 
-func (m *MTProto) ChannelsEditAbout(channel TL, about string) (*TL,error){
+func (m *MTProto) ChannelsEditAbout(channel TL, about string) (*TL, error) {
 	return m.InvokeSync(TL_channels_editAbout{
-		Channel:channel,
-		About:about,
+		Channel: channel,
+		About:   about,
 	})
 }
 
-func (m *MTProto) ChannelsEditTitle(channel TL, title string) (*TL,error){
+func (m *MTProto) ChannelsEditTitle(channel TL, title string) (*TL, error) {
 	return m.InvokeSync(TL_channels_editTitle{
-		Channel:channel,
-		Title:title,
+		Channel: channel,
+		Title:   title,
 	})
 }
 
-func (m *MTProto) ChannelsInviteToChannel(channel TL,users []TL) (*TL,error){
+func (m *MTProto) ChannelsInviteToChannel(channel TL, users []TL) (*TL, error) {
 	return m.InvokeSync(TL_channels_inviteToChannel{
-		Channel:channel,
-		Users:users,
+		Channel: channel,
+		Users:   users,
 	})
 }
 
-func (m *MTProto) ChannelsToggleInvites(channel TL,enabled TL) (*TL,error){
+func (m *MTProto) ChannelsToggleInvites(channel TL, enabled TL) (*TL, error) {
 	return m.InvokeSync(TL_channels_toggleInvites{
-		Channel:channel,
-		Enabled:enabled,
+		Channel: channel,
+		Enabled: enabled,
 	})
 }
 
-func (m *MTProto) ChannelsToggleSignatures(channel TL,enabled TL) (*TL,error){
+func (m *MTProto) ChannelsToggleSignatures(channel TL, enabled TL) (*TL, error) {
 	return m.InvokeSync(TL_channels_toggleSignatures{
-		Channel:channel,
-		Enabled:enabled,
+		Channel: channel,
+		Enabled: enabled,
 	})
 }
-
->>>>>>> 92956a3d23a08ef76cf2b189cc904a596431fc20
